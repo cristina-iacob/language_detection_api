7d813ac47b8a1850093e0555f227278cede78341
Adam Newlyn <lord.beaker@gmail.com>
2019-11-07 15:26:55 -0800

    now loads language model

--- web/app.py
+++ web/app.py
@@ -2,28 +2,65 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import send_from_directory
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Dropout
 
+import numpy as np
 import pandas as pd
 import os
 
-# Test FLASK with premade model
 from tensorflow.keras.models import load_model
-model_1 = load_model("./voice_model_trained.h5")
 
-from sklearn.linear_model import LogisticRegression
+max_letters=12
+char_count=104
+label_names=['english', 'french', 'german', 'romanian']
 
-voice = pd.read_csv(os.path.join('.','voice.csv'))
-X = voice.drop("label", axis=1)
-y = voice["label"]
+network = Sequential()
+network.add(Dense(200, input_dim=(char_count*max_letters)-1, activation='sigmoid'))
+network.add(Dense(150, activation='sigmoid'))
+network.add(Dense(100, activation='sigmoid'))
+network.add(Dense(100, activation='sigmoid'))
+network.add(Dense(len(label_names), activation='softmax'))
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, stratify=y)
+network.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-classifier = LogisticRegression()
-classifier
-classifier.fit(X_train, y_train)
+network = load_model("./lang_detect.hdf5")
 
-predictions = classifier.predict(X_test.head(1))
+def convert_dic_to_vector(dic, max_word_length):
+    new_list = []
+    for word in dic:
+        vec = ''
+        n = len(word)
+        for i in range(n):
+            current_letter = word[i]
+            ind = ord(current_letter)-97
+            #ind = ord(current_letter)
+            placeholder = (str(0)*ind) + str(1) + str(0)*((char_count-1)-ind)
+            vec = vec + placeholder
+        if n < max_word_length:
+            excess = max_word_length-n
+            vec = vec +str(0)*char_count*excess
+        new_list.append(vec)
+    print(len(new_list))
+    return new_list
+
+
+dic = []
+dic.append("hello")
+vct_str = convert_dic_to_vector(dic, max_letters-1)
+vct = np.zeros((1, (char_count * max_letters)-1))
+count = 0
+print(len(vct_str[0]))
+for digit in vct_str[0]:
+    vct[0,count] = int(digit)
+    count += 1
+prediction_vct = network.predict(vct)
+
+langs = list(label_names)
+for i in range(len(label_names)):
+    lang = langs[i]
+    score = prediction_vct[0][i]
+    print(lang + ': ' + str(round(100*score, 2)) + '%')
 
 # Flask Setup
 app = Flask(__name__, static_url_path='')
